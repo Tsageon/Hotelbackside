12edd46f3bb5e86daa1869786d77124b798d28fa
Tsageon <sagaetshepo@gmail.com>
2024-11-18 13:35:22 +0200

    CORSSSSSSSSSSSSSSSERORRORORORORR

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 
-
 app.use(bodyParser.json());
+app.use(cors());
 
 
 const transporter = nodemailer.createTransport({
